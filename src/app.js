0b3c8138c0643fab6809d88e15a57c5283ea1c16
PranjalBarns <happybarnwal007@gmail.com>
2025-01-17 07:16:19 +0530

    password hash and login route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const connectToDb = require("./../db/database");
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const User = require("./../models/user");
 
 const app = express();
@@ -9,28 +10,54 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const newUser = new User({
-    firstName: req.body.firstName,
-    lastName:  req.body.lastName,
-    emailId:  req.body.emailId,
-    password: req.body.password,
-    age: req.body.age,
-    about: req.body.about,
-    gender: req.body.gender,
-    skills: req.body.skills,
-    photoURL: req.body.photoURL,
-  });
   try {
+    let hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailId: req.body.emailId,
+      password: hashedPassword,
+      age: req.body.age,
+      about: req.body.about,
+      gender: req.body.gender,
+      skills: req.body.skills,
+      photoURL: req.body.photoURL,
+    });
     const savedUser = await newUser.save();
     res.send({
-        id:savedUser,
-        message:"Data saved to DB"
-    })
+      id: savedUser,
+      message: "Data saved to DB",
+    });
   } catch (err) {
     res.status(400).send({
-        errorMessage:err,
-        message:"Unable to save data from db"
-    })
+      errorMessage: err.message,
+      message: "Unable to save data from db",
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    console.log(user);
+
+    if (!user) throw new Error("No user found");
+    const isSame = await bcrypt.compare(password, user.password);
+    if (isSame) {
+      res.send({
+        message: "User successfully loggedin",
+        user
+      });
+    } else {
+      throw new Error("Error while loggin in");
+    }
+  } catch (err) {
+    res.send({
+      errorMessage: err.message,
+      message: "Error occured while login",
+    });
   }
 });
 
